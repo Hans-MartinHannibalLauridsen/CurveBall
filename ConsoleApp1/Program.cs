2bb9579df5485b994a487dcf02e7a0eefb7fe0b0
Hannibal <Hans-MartinHannibalLauridsen@hotmail.com>
2020-01-23 19:25:00 +0100

    Created rouge parameters with Bounty Castle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Math;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== CurveBall ===");
+            
+            var parameters = ECNamedCurveTable.GetByName("prime256v1");
+            
+            var publicKey = parameters.Curve.DecodePoint(new byte[]{});
+            var privateKey = parameters.Curve.FromBigInteger(BigInteger.Two);
+            var generator = publicKey.Multiply(privateKey.Invert().ToBigInteger());
+            
+
+        }
+    }
+}
